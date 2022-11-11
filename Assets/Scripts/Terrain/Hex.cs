33e89d27f3c048dcd58784a094783260ac921edf
Gatien Vilain <gatien.vilain@outlook.fr>
2022-11-11 10:56:14 +0100

    Merge branch 'main' into Update-the-Characters-branch

---
+++ Assets/Scripts/Terrain/Hex.cs
@@ -0,0 +1,182 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using UnityEditor.ShaderGraph.Internal;
+using UnityEngine;
+using UnityEngine.Assertions.Must;
+/// <summary>
+/// The Hex class defines the grid position, world space position, size,
+/// neighbours, etc ... of a Hex Tile. However, it does NOT interact with
+/// Unity directly in any way.
+/// </summary>
+public class Hex
+{
+    //Q + R + S = 0
+    // S = -(Q+R)
+
+    public readonly int Q; //Column
+    public readonly int R; //Row
+    public readonly int S; //Size
+
+    //Data for map generation and maybe in-game effects
+    public float Elevation;
+    public float Moisture;
+
+    //TODO : Need some kind of property to track hex type (plains, grasslands, etc...)
+    //TODO : Beed property to track hex detail (forest, mine, farm, etc...)
+
+    float radius = 1f;
+    public readonly HexMap hexMap;
+    
+    HashSet<Unit> units;
+
+    static readonly float WIDTH_MULTIPLIER = Mathf.Sqrt(3) / 2;
+
+    public Hex(HexMap hexMap, int q, int r)
+    {
+        this.Q = q;
+        this.R = r;
+        this.S = -(q+r);
+        this.hexMap = hexMap;
+    }
+
+    /// <summary>
+    /// Returns the world-space position of this hex
+    /// </summary>
+    public Vector3 Position()
+    {
+        return new Vector3(
+            HexHorizontalSpacing() * (this.Q + this.R / 2f),
+            0,
+            HexVerticalSpacing() * this.R
+        );
+
+        //return new Vector3(HexHorizontalSpacing() * (this.Q + this.R/2f), 0, HexVerticalSpacing() * this.R);
+    }
+
+    public Vector3 PositionFromCamera()
+    {
+        return hexMap.GetHexPosition(this);
+    }
+
+    public Vector3 PositionFromCamera(Vector3 cameraPosition, float numberRows, float numberColumns)
+    {
+        float mapHeight = numberRows * HexVerticalSpacing();
+        float mapWidth = numberColumns * HexHorizontalSpacing();
+
+        Vector3 position = Position();
+        if(hexMap.allowWrapEastWest)
+        {
+            float howManyWidthsFromCamera = (position.x - cameraPosition.x) / mapWidth;
+
+            //We want howManyWidthsFromCamera to be between -0.5 to 0.5
+            /*if(Mathf.Abs(howManyWidthsFromCamera) <= 0.5f)
+            {
+                return position;
+            }*/
+
+            if (howManyWidthsFromCamera > 0)
+            {
+                howManyWidthsFromCamera += 0.5f;
+            }
+            else
+            {
+                howManyWidthsFromCamera -= 0.5f;
+            }
+
+            int howManyWidthToFix = (int)howManyWidthsFromCamera;
+            position.x -= howManyWidthToFix * mapWidth;
+        }
+
+        if(hexMap.allowWrapNorthSouth)
+        {
+            float howManyWidthsFromCamera = (position.z - cameraPosition.z) / mapHeight;
+
+            //We want howManyWidthsFromCamera to be between -0.5 to 0.5
+            /*if(Mathf.Abs(howManyWidthsFromCamera) <= 0.5f)
+            {
+                return position;
+            }*/
+
+            if (howManyWidthsFromCamera > 0)
+            {
+                howManyWidthsFromCamera += 0.5f;
+            }
+            else
+            {
+                howManyWidthsFromCamera -= 0.5f;
+            }
+
+            int howManyWidthToFix = (int)howManyWidthsFromCamera;
+            position.z -= howManyWidthToFix * mapHeight;
+        }
+        
+        return position;
+
+    }
+
+    public float HexHeight()
+    {
+        return radius * 2;
+    }
+
+    public float HexWidth()
+    {
+        return WIDTH_MULTIPLIER * HexHeight();
+    }
+
+    public float HexVerticalSpacing()
+    {
+        return HexHeight() * 0.75f;
+    }
+
+    public float HexHorizontalSpacing()
+    {
+        return HexWidth();
+    }
+
+    public static float Distance(Hex a, Hex b)
+    {
+        int dQ = Mathf.Abs(a.Q - b.Q);
+        if(dQ > a.hexMap.numberRows/2)
+        {
+            dQ = a.hexMap.numberColumns - dQ;
+        }
+        return
+            Mathf.Max(
+            dQ,
+            Mathf.Abs(a.R - b.R),
+            Mathf.Abs(a.S - b.S)
+            );
+    }
+
+    public void AddUnit(Unit unit)
+    {
+        if(units == null)
+        {
+            units = new HashSet<Unit>();
+
+        }
+        units.Add(unit);
+    }
+
+    public void RemoveUnit(Unit unit)
+    {
+        if(units != null)
+        {
+            units.Remove(unit);
+        }
+        
+    }
+
+    public Unit[] getUnits()
+    {
+        return units.ToArray();
+    }
+
+    public int BaseMovementCost()
+    {
+        //TODO : Factor in terrain type & features
+        return 1;
+    }
+}
---
+++ Assets/Scripts/Terrain/HexMap.cs
@@ -0,0 +1,266 @@
+using JetBrains.Annotations;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class HexMap : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        GenerateMap();
+    }
+
+    void Update()
+    {
+        //TESTING : Press spacebar to advance to next turn
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            if(units != null)
+            {
+                foreach(Unit unit in units)
+                {
+                    unit.DoTurn();
+                }
+            }
+        }
+    }
+
+    [SerializeField] GameObject hexTile;
+
+    [SerializeField] Mesh MeshWater;
+    [SerializeField] Mesh MeshFlat;
+    [SerializeField] Mesh MeshHill;
+    [SerializeField] Mesh MeshMountain;
+
+    [SerializeField] GameObject ForestPrefab;
+    [SerializeField] GameObject JunglePrefab;
+
+    [SerializeField] Material MatOcean;
+    [SerializeField] Material MatPlains;
+    [SerializeField] Material MatMountains;
+    [SerializeField] Material MatGrassLands;
+    [SerializeField] Material MatDesert;
+
+    public GameObject UnitDwarfPrefab;
+
+    protected float HeightMountain = 0.85f;
+    protected float HeightHill = 0.6f;
+    protected float HeightFlat = 0.0f;
+    public bool allowWrapEastWest = true;
+    public bool allowWrapNorthSouth = false;
+
+    protected float MoistureJungle = 0.66f;
+    protected float MoistureForest = 0.33f;
+    protected float MoistureGrasslands = 0f;
+    protected float MoisturePlains = -0.5f;
+
+
+    public int numberRows = 30;
+    public int numberColumns = 60; 
+    private GameObject hexGO;
+
+    private Hex[,] hexes;
+    private Dictionary<Hex, GameObject> hexToGameObjectMap;
+
+    private HashSet<Unit> units;
+    private Dictionary<Unit, GameObject> unitToGameObjectMap;
+
+    public Hex getHexeAt(int x, int y)
+    {
+        if(hexes == null)
+        {
+            //throw new UnityException("Hexes array not yet extentiated !");
+            Debug.Log("Hexes array not yet extentiated !");
+            return null;
+        }
+
+        if(allowWrapEastWest)
+        {
+            x = x % numberColumns;
+            if (x < 0)
+            {
+                x += numberColumns;
+            }
+        }
+        
+        /*if (y < 0)
+        {
+            y += numberColumns;
+        }*/
+        try
+        {
+            return this.hexes[x, y];
+        }
+        catch
+        {
+            Debug.LogError("GetHexAt: " + x + "," + y);
+            return null;
+        }
+        
+    }
+
+    public Vector3 GetHexPosition(int q, int r)
+    {
+        Hex h = getHexeAt(q, r);
+        return GetHexPosition(h);
+    }
+
+    public Vector3 GetHexPosition(Hex hex)
+    {
+        return hex.PositionFromCamera(Camera.main.transform.position, numberRows, numberColumns);
+    }
+    virtual public void GenerateMap()
+    {
+        hexes = new Hex[numberColumns, numberRows];
+        hexToGameObjectMap = new Dictionary<Hex, GameObject>();
+        for (int column = 0; column< numberColumns;column++)
+        {
+            for (int row =0; row<numberRows; row++ )
+            {
+                //Instantiate a Hex
+                Hex h = new Hex(this, column, row);
+                Vector3 pos = h.PositionFromCamera(Camera.main.transform.position, numberRows, numberColumns);
+                h.Elevation = -0.5f;
+                h.Moisture = 0f;
+                hexes[column, row] = h;
+
+                
+
+                hexGO = (GameObject)Instantiate(hexTile, pos, Quaternion.identity, this.transform);
+                hexGO.GetComponent<HexComponent>().hex = h;
+                hexGO.GetComponent<HexComponent>().hexMap = this;
+                hexToGameObjectMap[h] = hexGO;
+
+                hexGO.GetComponentInChildren<TextMesh>().text = string.Format("{0},{1}", column, row);
+                MeshRenderer mr = hexGO.GetComponentInChildren<MeshRenderer>();
+                mr.material = MatOcean;
+
+                MeshFilter mf = hexGO.GetComponentInChildren<MeshFilter>();
+                mf.mesh = MeshWater;
+            }
+            
+        }
+        UpdateHexVisuals();
+
+        //StaticBatchingUtility.Combine(this.gameObject);
+
+        
+    }
+
+    public void UpdateHexVisuals()
+    {
+        for (int column = 0; column < numberColumns; column++)
+        {
+            for (int row = 0; row < numberRows; row++)
+            {
+                //Debug.Log("Column :" + column);
+                //Debug.Log("Row :" + row);
+                Hex h = hexes[column, row];
+                GameObject hexGO = hexToGameObjectMap[h];
+                MeshRenderer mr = hexGO.GetComponentInChildren<MeshRenderer>();
+                MeshFilter mf = hexGO.GetComponentInChildren<MeshFilter>();
+
+                if(h.Elevation >= HeightFlat)
+                {
+                    if (h.Moisture >= MoistureJungle)
+                    {
+                        mr.material = MatGrassLands;
+                        Vector3 p = hexGO.transform.position;
+                        if(h.Elevation >= HeightHill)
+                        {
+                            p.y += 0.25f;
+                        }
+                        //TODO : Spawn Jungle
+                        GameObject.Instantiate(JunglePrefab, p, Quaternion.identity, hexGO.transform);
+                    }
+
+
+                    else if (h.Moisture >= MoistureForest)
+                    {
+                        mr.material = MatGrassLands;
+                        Vector3 p = hexGO.transform.position;
+                        if (h.Elevation >= HeightHill)
+                        {
+                            p.y += 0.25f;
+                        }
+                        GameObject.Instantiate(ForestPrefab, p, Quaternion.identity, hexGO.transform);
+                    }
+
+                    else if (h.Moisture >= MoistureGrasslands)
+                    {
+                        mr.material = MatGrassLands;
+                    }
+
+                    else if (h.Moisture >= MoisturePlains)
+                    {
+                        mr.material = MatPlains;
+
+                    }
+
+                    else
+                    {
+                        mr.material = MatDesert;
+                    }
+                }
+
+                if (h.Elevation >= HeightMountain)
+                {
+                    mr.material = MatMountains;
+                    mf.mesh = MeshMountain;
+                }
+
+                else if (h.Elevation >= HeightHill)
+                {
+                    mf.mesh = MeshHill;
+                }
+
+                else if (h.Elevation >= HeightFlat)
+                {
+                    mf.mesh = MeshWater;
+                }
+
+                else
+                {
+                    mr.material = MatOcean;
+                    mf.mesh = MeshWater;
+                }
+            }
+        }
+    }
+
+    public  Hex[] getHexesWithinRangeOf(Hex centerHex, int range)
+    {
+        List<Hex> results = new List<Hex>();
+        for(int dx = -range; dx < range-1; dx++)
+        {
+            for(int dy = Mathf.Max(-range+1, -dx-range); dy < Mathf.Min(range, -dx+range-1); dy++)
+            {
+                results.Add(getHexeAt(centerHex.Q +dx, centerHex.R +dy));
+            }
+        }
+
+        return results.ToArray();
+    }
+
+    public void SpawnUnitAt(Unit unit, GameObject prefab, int q, int r)
+    {
+        if(units == null)
+        {
+            units = new HashSet<Unit>();
+            unitToGameObjectMap = new Dictionary<Unit, GameObject>();
+        }
+
+
+        Hex myHex = getHexeAt(q, r);
+        GameObject myHexGO = hexToGameObjectMap[myHex];
+        unit.SetHex(myHex);
+        GameObject unitGO = Instantiate(prefab, myHexGO.transform.position, Quaternion.identity, myHexGO.transform);
+        unit.OnUnitMoved += unitGO.GetComponent<UnitView>().OnUnitMoved;
+
+        units.Add(unit);
+        unitToGameObjectMap[unit] = unitGO;
+    }
+
+}
