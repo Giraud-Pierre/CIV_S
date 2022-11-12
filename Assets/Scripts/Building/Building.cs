05307904c19f31444d45fa0c8b4f35252188634e
Giraud-Pierre <90455112+Giraud-Pierre@users.noreply.github.com>
2022-11-12 17:23:37 +0100

    Doturn1

--- Assets/Scripts/Building/Building.cs
+++ Assets/Scripts/Building/Building.cs
@@ -5,11 +5,32 @@ using UnityEngine;
 public class Building
 {
     public string name;
-    public int cost;
 
-    public Building(string name, int cost)
+    //0 pour un centre ville, 1 pour un moulin, 2 pour une cabane de bucheron, 3 pour une mine
+    public int type;
+
+    //Contenu dans BuildingPokedex.buildings[buildingType] avec dans l'ordre nourriture, bois, pierre.
+    public List<int> cost = new List<int>(3); 
+
+    public Hex hex; //Case du bâtiment
+    public List<Unit> queue = new List<Unit>(); //Liste des unités pour le centre ville
+
+    public Building(int buildingType, BuildingPokedex pokedex, Hex newHex)
     {
-        this.name = name;
-        this.cost = cost;
-    }   
+        this.type = buildingType;
+        this.name = pokedex.buildings[type].name;
+        this.cost = pokedex.buildings[type].cost;
+        hex = newHex;
+    }
+
+    public void DoTurn()
+    {
+        switch (type)
+        {
+            case 0:
+                //doturn for citycenter
+                break;
+
+        }
+    }
 }
---
+++ Assets/Scripts/Building/BuildingSheet.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu]
+public class BuildingSheet : ScriptableObject
+{
+    public new string name;
+    public List<int> cost = new List<int>(3);
+}
